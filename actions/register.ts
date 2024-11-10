01a3c6ab1fc393c2a70fc950398141473e4a611c
ifaamrillah <amrillah.work@gmail.com>
2024-11-10 13:39:13 +0700

    feat: add create user api

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,33 @@
 "use server";
 
 import { z } from "zod";
+import bcrypt from "bcrypt";
 
 import { RegisterSchema } from "@/schemas";
 
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
+
 export async function register(values: z.infer<typeof RegisterSchema>) {
   const validatedFields = RegisterSchema.safeParse(values);
-  if (!validatedFields) return { error: "Invalid fields!" };
+  if (!validatedFields.success) return { error: "Invalid fields!" };
 
-  return { success: "Email sent!" };
+  const { name, email, password } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) return { error: "Email already user!" };
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 }
