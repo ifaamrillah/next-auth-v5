f29f4bbab9298efd6d2ea3e85ccb6b7c697a7683
ifaamrillah <amrillah.work@gmail.com>
2024-11-12 10:30:18 +0700

    feat: add auto email verified OAuth

--- auth.ts
+++ auth.ts
@@ -8,6 +8,18 @@ import { getUserById } from "./data/user";
 import authConfig from "./auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          emailVerified: new Date(),
+        },
+      });
+    },
+  },
   callbacks: {
     async session({ token, session }) {
       if (token.sub && session.user) {
